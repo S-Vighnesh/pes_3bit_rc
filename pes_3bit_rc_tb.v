2d1f667661168e04ddede89160d9fffde68312b6
S-Vighnesh <137196908+S-Vighnesh@users.noreply.github.com>
2023-09-01 22:13:52 +0530

    Update and rename iiitb_3bit_rc_tb.v to pes_3bit_rc_tb.v

--- pes_3bit_rc_tb.v
+++ pes_3bit_rc_tb.v
@@ -1,9 +1,9 @@
 `timescale 1ns/1ps
-module iiitb_3bit_rc_tb;
+module pes_3bit_rc_tb;
 	reg clk;
 	reg ori;
 	wire [2:0] count;
-	iiitb_3bit_rc rc(clk, ori, count);
+	pes_3bit_rc rc(clk, ori, count);
 	initial
 	begin
 		clk = 0;
@@ -17,8 +17,8 @@ module iiitb_3bit_rc_tb;
 	end
 	initial
 	begin
-		$dumpfile("iiitb_3bit_rr_out.vcd");
-		$dumpvars(0, iiitb_3bit_rc_tb);
+		$dumpfile("pes_3bit_rr_out.vcd");
+		$dumpvars(0, pes_3bit_rc_tb);
 		#20
 		ori = 0;
 		#200;
